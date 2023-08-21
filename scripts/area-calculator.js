72dbf9235a070f41b9c6c65262cae713902ab60f
SalmanJunaed <salman9blog@gmail.com>
2023-08-21 16:32:13 +0600

    calculate entry

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -5,8 +5,8 @@ function calculateTriangleArea(){
     const baseField = document.getElementById('triangle-base');
     const baseValueText = baseField.value;
     // console.log(baseValueText);
-    const base = parseFloat(baseValueText);
-    console.log(base);
+    const bases = parseFloat(baseValueText);
+    console.log(bases);
     baseField.value='';
 
     //get Triangle Height Value
@@ -17,7 +17,7 @@ function calculateTriangleArea(){
     heightField.value='';
 
     // calculate values
-    const area = 0.5 * base * height;
+    const area = 0.5 * bases * height;
     console.log(area);
 
     // show triangle area
@@ -46,26 +46,40 @@ function calculateRactangleArea(){
     // calculate values
     const area = width * length;  
     console.log(area);
+    
+    //validate Input: width and length
+    if(isNaN(width)||isNaN(length)){
+        // console.log('width is not a number');
+        alert('Please insert a number');
+        return;
+    }
 
     // show Ractangle area
     const areaSpan = document.getElementById('ractangle-area');
     areaSpan.innerText = area;
 }
 
-
+//Parallelogram
 // reusable function --> reduce duplicate code
 function calculateParallelogramArea(){
     const base = getInputValue('parallelogram-base');
     // console.log(base);
-
     const height = getInputValue('parallelogram-base');
     // console.log(height);
-
+    //validate number
+    if(isNaN(base)||isNaN(height)){
+        alert('Please insert a number');
+        return;
+    }
     // calculate values
-    const area = base * height;  
+    const area = base * height; 
     // console.log(area);
-    setElementInnerText('parallelogram-area', area);
+    setElementInnerText('parallelogram-area',area);
+    // add to calculation entry
+    addToCalculateEntry('parallelogram', area);
 }
+
+
 // Ellipse
 function calculateEllipseArea(){
     const majorRadius = getInputValue('ellipse-major-radius');
@@ -89,3 +103,29 @@ function setElementInnerText(elementID, area){
     const element = document.getElementById(elementID);
     element.innerText = area;
 }
+// add to calculation entry
+function addToCalculateEntry(areaType, area){
+    console.log(areaType+ ' ' + area);
+    const calculationEntry = document.getElementById('calculation-entry');
+    
+    const p = document.createElement('p');
+    p.innerHTML = areaType+' '+area;
+
+    calculationEntry.appendChild(p);
+}
+// add to calculation entry
+/**
+ * 1. get the element where you want to add the dynamic HTML
+ * 2. create an element you want to add
+ * 3. if need to add some class
+ * 4. set innerHTML. it could be dynamic Template string
+ * 5. appand the created element as aq child  
+ */
+
+//Data Validation
+/**
+ * 1.set the proper type of the input fields. (number, data, email, password)
+ * 2. check 'typeof'
+ * 3. 'parseFloat' 'parseInt'
+ * 4. 'isNan()' NaN means - not a number. NaN is checking whether the input is not a number or not.
+ */
